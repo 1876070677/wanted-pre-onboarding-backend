9d9997473aaff886ab37081598501775ada41998
1876070677 <kdl5001@catholic.ac.kr>
2024-08-07 03:25:47 +0900

    first commit

---
+++ src/main/java/com/example/wanted_ex/WantedExApplication.java
@@ -0,0 +1,13 @@
+package com.example.wanted_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wanted_ex/WantedExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wanted_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
